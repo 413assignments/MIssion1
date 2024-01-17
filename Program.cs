56d203d7a077cfe51df1c384bf135663ac1b6399
David Bassett <112457273+Yylium@users.noreply.github.com>
2024-01-17 16:52:08 -0700

    mission1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Dice;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Roll roll = new Roll();
+
+        int[] results;
+        int rolls;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!" +
+            "\n\n" +
+            "How many dice rolls would you like to simulate?");
+
+        rolls = int.Parse(System.Console.ReadLine());
+
+        results = roll.diceRoll(rolls);
+
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS" +
+            "\nEach \"*\" represents 1% of the total number of rolls." +
+            "\nTotal number of rolls = " + rolls.ToString() + ".\n");
+
+        for (int i = 0; i < 11; i++)
+        {
+            string asterisks = "";
+            for (int x = 0; x < 100 * results[i] / rolls; x++)
+            {
+                asterisks += "*";
+            }
+
+            System.Console.WriteLine((i + 2).ToString() + ": " + asterisks);
+        }
+
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!\n\n\n");
+    }
+}
+
---
+++ Roll.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    internal class Roll
+    {
+
+        private int[] results;
+
+        public Roll()
+        {
+            results = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        }
+
+
+        public int[] diceRoll(int x)
+        {
+           
+            Random rnd = new Random();
+
+            for (int i = 0; i < x; i++)
+            {
+                results[rnd.Next(1, 7) + rnd.Next(1, 7) - 2] += 1;
+            }
+
+            return results;
+            
+        }
+    }
+}
+
